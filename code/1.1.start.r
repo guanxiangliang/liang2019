38b86b87e28ab90a38b846098f5d8149799f2c20
Guanxiang Liang <guanxiang@microb120.MED.UPENN.EDU>
2019-04-25 11:12:28 -0400

    update

--- code/1.1.start.r
+++ code/1.1.start.r
@@ -1,5 +1,6 @@
 #### Libraries ####
 library(taxonomizr)### convert accession id to tax id and to names ###
+dir.create('input/database/ac2tax')
 prepareDatabase('input/database/ac2tax/accessionTaxa.sql',types = c("nucl_gb", "nucl_wgs","prot"))
 #file.remove(list.files('./input/database/ac2tax','accession2taxid.gz$'))
 #file.remove(list.files('./input/database/ac2tax','dmp$'))
